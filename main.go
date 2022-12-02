180cee8d39fdd459e3e1a58db21748192245b22b
Arnaud Berthomier <oz@cyprio.net>
2022-12-02 10:12:52 -0600

    Bump version: 0.6.2

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ import (
 )
 
 // CurrentVersion represents the current build version.
-const CurrentVersion = "0.6.1"
+const CurrentVersion = "0.6.2"
 
 var (
 	term              = termenv.ColorProfile()
