7bd474c9e636c0c361965ea91276993317e8fc83
https://github.com/jnd-au <jnd-au@users.noreply.github.com>
2023-01-16 16:45:33 +1100

    v0.6.3 view.go: Fix starting hour for timezones with non-hourly offsets

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ import (
 )
 
 // CurrentVersion represents the current build version.
-const CurrentVersion = "0.6.2"
+const CurrentVersion = "0.6.3"
 
 var (
 	term              = termenv.ColorProfile()
