6ebaa349d576ee952bccff533c4b51c035ddfcdd
Arnaud Berthomier <oz@cyprio.net>
2024-01-09 18:35:24 -0600

    Bump version: 0.7.0
    Let's follow semver a bit. :)

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ import (
 )
 
 // CurrentVersion represents the current build version.
-const CurrentVersion = "0.6.4"
+const CurrentVersion = "0.7.0"
 
 var (
 	term              = termenv.ColorProfile()
