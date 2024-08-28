4f0146dafbea613859e824a30247cf497fc39c2d
Justin Wyne <1986068+wyne@users.noreply.github.com>
2024-08-27 22:04:28 -0700

    Load settings from config file

---
+++ config_file.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/pelletier/go-toml/v2"
+)
+
+// Config represents the entire TOML configuration
+type ConfigFile struct {
+	Header  string            `toml:"header"`
+	Zones   []ConfigFileZone  `toml:"zones"`
+	Keymaps ConfigFileKeymaps `toml:"keymaps"`
+}
+
+// Zone represents a single zone entry in the TOML file
+type ConfigFileZone struct {
+	ID   string `toml:"id"`
+	Name string `toml:"name"`
+}
+
+// Keymaps represents the key mappings in the TOML file
+type ConfigFileKeymaps struct {
+	PrevHour   []string `toml:"prev_hour"`
+	NextHour   []string `toml:"next_hour"`
+	PrevDay    []string `toml:"prev_day"`
+	NextDay    []string `toml:"next_day"`
+	PrevWeek   []string `toml:"prev_week"`
+	NextWeek   []string `toml:"next_week"`
+	ToggleDate []string `toml:"toggle_date"`
+	OpenWeb    []string `toml:"open_web"`
+	Now        []string `toml:"now"`
+}
+
+func setupZone(now time.Time, zoneConf ConfigFileZone) (*Zone, error) {
+	name := zoneConf.Name
+	dbName := zoneConf.ID
+
+	loc, err := time.LoadLocation(dbName)
+	if err != nil {
+		return nil, fmt.Errorf("looking up zone %s: %w", dbName, err)
+	}
+	if name == "" {
+		name = loc.String()
+	}
+	then := now.In(loc)
+	shortName, _ := then.Zone()
+	return &Zone{
+		DbName: loc.String(),
+		Name:   fmt.Sprintf("(%s) %s", shortName, name),
+	}, nil
+}
+
+func LoadConfigFile() (*Config, error) {
+	conf := Config{
+		Zones: DefaultZones,
+	}
+
+	// Expand the ~ to the home directory
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+
+	configFilePath := filepath.Join(homeDir, ".config", "tz", "conf.toml")
+
+	// Read the TOML file
+	configFile, err := os.ReadFile(configFilePath)
+	if err != nil {
+		panic(err)
+	}
+
+	// Unmarshal the TOML data into the Config struct
+	var config ConfigFile
+	err = toml.Unmarshal(configFile, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	// Use the config values
+	fmt.Printf("Header: %s\n", config.Header)
+	fmt.Println("Zones:")
+	for _, zone := range config.Zones {
+		fmt.Printf("  ID: %s, Name: %s\n", zone.ID, zone.Name)
+	}
+
+	zones := make([]*Zone, len(config.Zones)+1)
+
+	// Setup with Local time zone
+	localZoneName, _ := time.Now().Zone()
+	zones[0] = &Zone{
+		Name:   fmt.Sprintf("(%s) Local", localZoneName),
+		DbName: localZoneName,
+	}
+
+	// Add zones from config file
+	for i, zoneConf := range config.Zones {
+		zone, err := setupZone(time.Now(), zoneConf)
+		if err != nil {
+			return nil, err
+		}
+		zones[i+1] = zone
+	}
+	conf.Zones = zones
+
+	return &conf, nil
+}
--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/oz/tz
 
-go 1.17
+go 1.21.0
+
+toolchain go1.23.0
 
 require (
 	github.com/charmbracelet/bubbletea v0.22.1
@@ -17,6 +19,7 @@ require (
 	github.com/muesli/ansi v0.0.0-20211031195517-c9f0611b6c70 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/rivo/uniseg v0.3.4 // indirect
 	golang.org/x/sys v0.0.0-20220818161305-2296e01440c6 // indirect
 	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035 // indirect
