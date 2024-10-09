oz/tz

commit e4e6d44e288744024c5a881025fe413c6f0264f9

Use time color as highlighted bg color, right align time

view.go
@@ -47,7 +47,7 @@ func (m model) View() string {
 			out = out.Foreground(term.Color(hourColorCode(hour)))
 			// Cursor
 			if m.hour == i-startHour {
-				out = out.Background(term.Color("#00B67F"))
+				out = out.Background(term.Color(hourColorCode(hour)))
 				if hasDarkBackground {
 					out = out.Foreground(term.Color("#262626")).Bold()
 				} else {
@@ -77,7 +77,7 @@ func (m model) View() string {
 			datetime = zone.ShortDT()
 		}
 
-		zoneHeader := fmt.Sprintf("%s %s %s", zone.ClockEmoji(), normalTextStyle(zone.String()), dateTimeStyle(datetime))
+		zoneHeader := fmt.Sprintf("%s %-60s %76s", zone.ClockEmoji(), normalTextStyle(zone.String()), dateTimeStyle(datetime))
 
 		s += fmt.Sprintf("  %s\n  %s\n  %s\n", zoneHeader, hours.String(), dates.String())
 	}

commit a7ebf5abbfec42c5a25085a0ee5dddfa6c1e909d

Dynamic right-alignment of dates
- Use COLUMNS env var or dynamic terminal width (up to UIWidth max)
- Refresh each time, in case the user’s terminal is resized
- Fix #53 (regression since e4e6d44)

go.mod
@@ -21,4 +21,5 @@ require (
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035 // indirect
 	golang.org/x/text v0.3.8 // indirect
+	golang.org/x/tools v0.7.0 // indirect
 )

go.sum
@@ -65,4 +65,6 @@ golang.org/x/text v0.3.8/go.mod h1:E6s5w1FMmriuDzIBO73fBruAKo1PCIq6d2Q6DHfQ8WQ=
 golang.org/x/tools v0.0.0-20180917221912-90fa682c2a6e/go.mod h1:n7NCudcB/nEzxVGmLbDWY5pfWTLqBcC2KZ6jyYvM4mQ=
 golang.org/x/tools v0.0.0-20191119224855-298f0cb1881e/go.mod h1:b+2E5dAYhXwXZwtnZ6UAqBI28+e2cm9otk0dWdXHAEo=
 golang.org/x/tools v0.1.12/go.mod h1:hNGJHUnrk76NpqgfD5Aqm5Crs+Hm0VOH/i9J2+nxYbc=
+golang.org/x/tools v0.7.0 h1:W4OVu8VVOaIO0yzWMNdepAulS7YfoS3Zabrm8DOXXU4=
+golang.org/x/tools v0.7.0/go.mod h1:4pg6aUX35JBAogB10C9AtvVL+qowtN4pT3CGSQex14s=
 golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=

view.go
@@ -18,25 +18,45 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"strings"
 
 	"github.com/muesli/termenv"
+	xterm "golang.org/x/term"
 )
 
 // Width required to display 24 hours
 const UIWidth = 94
+const MinimumZoneHeaderPadding = 6
+const MaximumZoneHeaderColumns = UIWidth + MinimumZoneHeaderPadding
 
 func (m model) View() string {
 	s := normalTextStyle("\n  What time is it?\n\n").String()
+
+	zoneHeaderWidth := MaximumZoneHeaderColumns
+	envWidth, envErr := strconv.Atoi(os.Getenv("COLUMNS"))
+	if envErr == nil {
+		zoneHeaderWidth = min(envWidth, zoneHeaderWidth)
+	} else {
+		fd := int(os.Stdout.Fd())
+		if xterm.IsTerminal(fd) {
+			termWidth, _, termErr := xterm.GetSize(fd)
+			if termErr == nil {
+				zoneHeaderWidth = min(termWidth, zoneHeaderWidth)
+			}
+		}
+	}
 
 	// Show hours for each zone
 	for zi, zone := range m.zones {
 		hours := strings.Builder{}
 		dates := strings.Builder{}
+		currentTime := zone.currentTime(m.clock.t)
 
 		startHour := 0
 		if zi > 0 {
-			startHour = (zone.currentTime(m.clock.t).Hour() - m.zones[0].currentTime(m.clock.t).Hour()) % 24
+			startHour = (currentTime.Hour() - m.zones[0].currentTime(m.clock.t).Hour()) % 24
 		}
 
 		dateChanged := false
@@ -77,7 +97,12 @@ func (m model) View() string {
 			datetime = zone.ShortDT(m.clock.t)
 		}
 
-		zoneHeader := fmt.Sprintf("%s %-60s %76s", zone.ClockEmoji(m.clock.t), normalTextStyle(zone.String()), dateTimeStyle(datetime))
+		clockString := zone.ClockEmoji(m.clock.t)
+		zoneString := zone.String()
+		usedZoneHeaderWidth := termenv.String(clockString + zoneString + datetime).Width()
+		unusedZoneHeaderWidth := max(0, zoneHeaderWidth - usedZoneHeaderWidth - MinimumZoneHeaderPadding)
+		rightAlignmentSpace := strings.Repeat(" ", unusedZoneHeaderWidth)
+		zoneHeader := fmt.Sprintf("%s %s %s%s", clockString, normalTextStyle(zoneString), rightAlignmentSpace, dateTimeStyle(datetime))
 
 		s += fmt.Sprintf("  %s\n  %s\n  %s\n", zoneHeader, hours.String(), dates.String())
 	}

main_test.go
@@ -17,11 +17,33 @@
 package main
 
 import (
+	"regexp"
 	"strings"
 	"testing"
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/muesli/termenv"
+)
+
+var (
+	ansiControlSequenceRegexp = regexp.MustCompile(regexp.QuoteMeta(termenv.CSI) + "[^m]*m")
+	utcMinuteAfterMidnightTime = time.Date(
+		2017, // Year
+		11, // Month
+		5, // Day
+		0, // Hour
+		1, // Minutes
+		2, // Seconds
+		127, // Nanoseconds
+		time.UTC,
+	)
+	utcMinuteAfterMidnightModel = model{
+		zones:      DefaultZones[len(DefaultZones) - 1:],
+		clock:      *NewClock(utcMinuteAfterMidnightTime.Unix()),
+		isMilitary: true,
+		showDates:  true,
+	}
 )
 
 func getTimestampWithHour(hour int) int64 {
@@ -38,6 +60,15 @@ func getTimestampWithHour(hour int) int64 {
 		0, // Nanoseconds set to 0
 		time.Now().Location(),
 	).Unix()
+}
+
+func stripAnsiControlSequences(s string) string {
+	return ansiControlSequenceRegexp.ReplaceAllString(s, "")
+}
+
+func stripAnsiControlSequencesAndNewline(bytes []byte) string {
+	s := strings.TrimSuffix(string(bytes), "\n")
+	return ansiControlSequenceRegexp.ReplaceAllString(s, "")
 }
 
 func TestUpdateIncHour(t *testing.T) {

view_test.go
@@ -1,0 +1,83 @@
+/**
+ * This file is part of tz.
+ *
+ * tz is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * tz is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tz.  If not, see <https://www.gnu.org/licenses/>.
+ **/
+package main
+
+import (
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+
+	"golang.org/x/tools/txtar"
+)
+
+func TestRightAlignment(t *testing.T) {
+	testDataFile := "testdata/view/test-right-alignment.txt"
+	testData, err := txtar.ParseFile(testDataFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := stripAnsiControlSequencesAndNewline(testData.Files[0].Data)
+
+	tests := []struct {
+		columns int
+	}{
+		{columns: 1},
+		{columns: 80},
+		{columns: 999},
+	}
+
+	originalColumns := os.Getenv("COLUMNS")
+	var observations []string
+	for _, test := range tests {
+		os.Setenv("COLUMNS", strconv.Itoa(test.columns))
+		observed := stripAnsiControlSequences(utcMinuteAfterMidnightModel.View())
+		observations = append(observations, observed)
+	}
+	os.Setenv("COLUMNS", originalColumns)
+
+	archive := txtar.Archive{
+		Comment: testData.Comment,
+		Files: []txtar.File{
+			{
+				Name: "expected",
+				Data: []byte(expected),
+			},
+			{
+				Name: "observed: narrow",
+				Data: []byte(observations[0]),
+			},
+			{
+				Name: "observed: medium",
+				Data: []byte(observations[1]),
+			},
+			{
+				Name: "observed: wide",
+				Data: []byte(observations[2]),
+			},
+		},
+	}
+	os.WriteFile(testDataFile, txtar.Format(&archive), 0666)
+
+	expectations := strings.Split(expected, "\n")
+	for i, test := range tests {
+		if !strings.Contains(observations[i], expectations[i]) {
+			t.Errorf("Expected %d-column alignment “%s”, but got: “%s”", test.columns, expectations[i], observations[i])
+		}
+	}
+}