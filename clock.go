39053d22edff9c0f404d5dd872b090f92b497766
James Devenish <jnd-au@users.noreply.github.com>
2024-10-09 16:45:52 +1100

    clock.go: Preserve timezone (fix 0bdd6e2 #57); fix -w with -when (fix 057fc06 #58)

--- clock.go
+++ clock.go
@@ -5,26 +5,37 @@ import "time"
 // Clock keeps track of the current time.
 type Clock struct {
 	t time.Time
+	isRealTime bool
 }
 
-// Make a new Clock.
-//
-// When "sec" is not t0, the returned Clock is set to this time,
-// assuming "sec" is a UNIX timestamp (cf. time.Unix).
-func NewClock(sec int64) *Clock {
+// A new Clock in the local timezone at the current time now.
+func NewClockNow() *Clock {
 	clock := new(Clock)
+	clock.t = time.Now()
+	clock.isRealTime = true
+	return clock
+}
 
-	if sec != 0 {
-		clock.t = time.Unix(sec, 0)
-	} else {
-		clock.t = time.Now()
-	}
+// A new Clock with the given time and timezone.
+func NewClockTime(t time.Time) *Clock {
+	clock := new(Clock)
+	clock.t = t
+	clock.isRealTime = false
+	return clock
+}
+
+// A new Clock in the local timezone at the `time.Unix` timestamp.
+func NewClockUnixTimestamp(sec int64) *Clock {
+	clock := new(Clock)
+	clock.t = time.Unix(sec, 0)
+	clock.isRealTime = false
 	return clock
 }
 
 // AddDays adds n days to the current date.
 func (c *Clock) AddDays(n int) {
 	c.t = c.t.AddDate(0, 0, n)
+	c.isRealTime = false
 }
 
 // AddDays adds n days to the current date and clears the minutes
@@ -40,6 +51,7 @@ func (c *Clock) AddHours(n int) {
 		c.t.Location(),
 	)
 	c.t = c.t.Add(time.Hour * time.Duration(n))
+	c.isRealTime = false
 }
 
 // Get the wrapped time.Time struct
